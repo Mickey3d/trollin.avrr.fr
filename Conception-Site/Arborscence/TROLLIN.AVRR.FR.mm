f02357192393fbb7ffc5b3f904a1e804a5dbc7ec
« Mickey » <mickey.welensky@gmail.com>
2017-01-21 00:07:34 +0100

    Actualisation du repertoire local avant push sur le remote github

---
+++ Conception-Site/Arborscence/TROLLIN.AVRR.FR.mm
@@ -0,0 +1,273 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="TROLLIN.AVRR.FR" FOLDED="false" ID="ID_1774687484" CREATED="1484840338264" MODIFIED="1484841344207" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="Menu Navigation" POSITION="right" ID="ID_31951848" CREATED="1484840515023" MODIFIED="1484840711055" TEXT_SHORTENED="true" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="4.0 pt" SHAPE_VERTICAL_MARGIN="4.0 pt">
+<edge COLOR="#3333ff" WIDTH="2"/>
+<node TEXT="Accueil" ID="ID_1585316050" CREATED="1484840961919" MODIFIED="1484852644669">
+<icon BUILTIN="gohome"/>
+<richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      souhaite la bienvenue au visiteur et lui indique ce que l&#8217;on peut trouver sur le site. Des liens sont pr&#233;vus pour acc&#233;der aux autres pages dans le corps de l&#8217;accueil. Un encart affichera un aper&#231;u de la derni&#232;re actualit&#233; post&#233;e par la ville.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Actualit&#xe9;s" ID="ID_1812079266" CREATED="1484841061560" MODIFIED="1484852641317"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Affiche la liste des billets d'actualit&#233;s -5 par page, renvois &#224; une page de pr&#233;sentation de l'actualit&#233;. Design commun pour ces pages. Les billets ne seront pas sign&#233;s par un auteur : ils seront tous post&#233;s au nom de la ville
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Actualit&#xe9; 1" ID="ID_1041022481" CREATED="1484841675270" MODIFIED="1484846072928" HGAP_QUANTITY="165.4999954849483 pt" VSHIFT_QUANTITY="-9.749999709427366 pt"/>
+<node TEXT="Actualit&#xe9; 2" ID="ID_1626000828" CREATED="1484841692225" MODIFIED="1484846062556" HGAP_QUANTITY="163.99999552965178 pt" VSHIFT_QUANTITY="-1.4999999552965178 pt"/>
+<node TEXT="Actualit&#xe9; 3" ID="ID_1027661915" CREATED="1484841707874" MODIFIED="1484846077046" HGAP_QUANTITY="163.99999552965178 pt" VSHIFT_QUANTITY="9.749999709427366 pt"/>
+</node>
+<node TEXT="Plus d&apos;Infos" ID="ID_967673136" CREATED="1484841095377" MODIFIED="1484852639922"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Doit contenir quelques &#233;l&#233;ments d&#233;taillant les diff&#233;rents types d&#8217;activit&#233;s culturelles organis&#233;es (mus&#233;e de la ville, marathon... pr&#233;voyez-en au moins 6 diff&#233;rentes). Il doit &#234;tre possible d&#8217;indiquer le nom de l&#8217;activit&#233;, une description et une image
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Histoire" ID="ID_1085739860" CREATED="1484846014090" MODIFIED="1484852651633" HGAP_QUANTITY="16.999999910593036 pt" VSHIFT_QUANTITY="17.99999946355821 pt"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mus&#233;e et Patrimoine
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Artisanat" ID="ID_114426336" CREATED="1484846088917" MODIFIED="1484852650621"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Place de la Bourse - Vannerie / Hachche/Bouclier / Bijoux / Cuir
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Gastronomie" ID="ID_1304531562" CREATED="1484846174191" MODIFIED="1484852649203"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      March&#233; de Glum / L'Artichaud
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Nature" ID="ID_1529281861" CREATED="1484846201401" MODIFIED="1484852652972"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Grotte / Randonn&#233;e
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Culture et Tradition" ID="ID_410158368" CREATED="1484846290553" MODIFIED="1484852654097"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Porcodrome
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Festivit&#xe9;s" ID="ID_343104981" CREATED="1484846320798" MODIFIED="1484852654862"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#8211; La f&#234;te de la Bi&#232;re printani&#232;re &#8211; La f&#234;te du Choix &#8211; La f&#234;te du Panier &#8211; La f&#234;te de l'Artichaut &#8211; La f&#234;te de l'Accolade
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Activit&#xe9;s" ID="ID_265510057" CREATED="1484841157804" MODIFIED="1484852637919"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Liste au moins 10 activit&#233;s &#224; venir dans le mois sous forme de tableau (avec date, titre et court descriptif). Les lignes doivent se colorer au survol de la souris pour qu&#8217;on puisse se rep&#233;rer. Un bouton &#171; Inscription &#187; &#224; droite de chaque ligne du tableau ouvre un formulaire permettant de saisir son nom. Une fois le formulaire envoy&#233;, la mairie re&#231;oit un email indiquant qu&#8217;une personne dont le nom est X s&#8217;est inscrite &#224; l&#8217;&#233;v&#233;nement Y
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Contact" ID="ID_1253562078" CREATED="1484841181768" MODIFIED="1484852638684"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      affiche un formulaire de contact structur&#233; avec les champs &#171; Titre du message &#187;, &#171; Votre nom &#187;, &#171; Votre date de naissance &#187;, &#171; Votre e-mail &#187;, une case &#224; cocher &#171; R&#233;sident de la ville &#187;, un menu d&#233;roulant &#171; Nationalit&#233; &#187; et un champ &#171; Message &#187; o&#249; on peut r&#233;diger son message. Un petit texte d&#8217;aide s&#8217;affiche sous le champ date de naissance pour dire que le format JJ/MM/AAAA est attendu. L&#8217;envoi du formulaire provoque l&#8217;envoi d&#8217;un email &#224; destination de la mairie.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Menu Footer" POSITION="left" ID="ID_1267517142" CREATED="1484841218531" MODIFIED="1484841841525" HGAP_QUANTITY="-168.99999454617517 pt" VSHIFT_QUANTITY="207.7499938085677 pt" COLOR="#ffcc66" STYLE="bubble">
+<edge COLOR="#0000ff"/>
+<node TEXT="Espace Administrateur" ID="ID_288994786" CREATED="1484841359989" MODIFIED="1484852646425" HGAP_QUANTITY="-56.49999789893633 pt" VSHIFT_QUANTITY="58.49999825656419 pt"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Acc&#232;s &#224; la page de connexion pour l'administration du site
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Mentions L&#xe9;gales" ID="ID_686935662" CREATED="1484841484122" MODIFIED="1484841503236" HGAP_QUANTITY="-110.49999628961098 pt" VSHIFT_QUANTITY="26.249999217689062 pt"/>
+</node>
+</node>
+</map>
